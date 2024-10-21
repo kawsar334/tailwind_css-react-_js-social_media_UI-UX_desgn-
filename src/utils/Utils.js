08cbed15fa1bfbba847d83974f64d2f965151ddc
kawsar firoz <kawsarfiroz11@gmail.com>
2024-10-21 16:48:25 +0300

    updated

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,7 +1,7 @@
 
 
 
-const URL = `https://social-media-backend-ochre.vercel.app/api/`;
 // const URL = `http://localhost:5000/api/`
+const URL = `https://social-media-backend-ochre.vercel.app/api/`;
 
 export default URL
